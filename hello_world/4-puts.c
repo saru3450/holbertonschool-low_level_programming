515e08a4688b9dd632b63b4fac905fe18dd979ca
saru3450 <9971@holbertonstudents.com>
2024-10-21 12:27:49 +0000

    =4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ *Return - always 0
+ */
+int main(void)
+{
+        punts("\""Programming is like building a multilingual puzzle);
+	return (0);
+}
