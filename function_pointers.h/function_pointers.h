b00c5d5274553b3739211551cf86e0f0ae4812a3
saru3450 <9971@holbertonstudents.com>
2024-11-18 17:38:44 +0000

    function_pointers.h

--- function_pointers.h/function_pointers.h
+++ function_pointers.h/function_pointers.h
@@ -1,12 +1,19 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include "function_pointers.h"
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int _putchar(char c);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
