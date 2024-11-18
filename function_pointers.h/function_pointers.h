270a561de115a265cedbe6efe9ea632b0eccff3c
saru34500 <9971@holbertonstudents.com>
2024-11-18 10:23:52 +0000

    function_pointers.h

---
+++ function_pointers.h/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+#include "function_pointers.h"
+#include <stdio.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
