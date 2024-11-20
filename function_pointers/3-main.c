3c613f8b745d4367991d7c01d2fd7f5672669412
saru3450 <9971@holbertonstudents.com>
2024-11-20 20:18:14 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,15 +3,16 @@
 #include <stdlib.h>
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
+ * main -  performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: ...
+ * Return: results of calc function execution
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	char *oprt;
+	int num1, num2;
 
 	if (argc != 4)
 	{
@@ -19,14 +20,22 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	oprt = (argv[2]);
+	num2 = atoi(argv[3]);
 
-	if (!oprt)
+	if (get_op_func(oprt) == NULL || oprt[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	if ((*oprt == '/' && num2 == 0) || (*oprt == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(oprt) (num1, num2));
 	return (0);
 }
