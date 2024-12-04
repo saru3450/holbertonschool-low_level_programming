7f1c647b9c5bd8b3d275318398da7eb768db7b1f
saru3450 <9971@holbertonstudents.com>
2024-12-04 09:43:13 +0000

    Write a function that adds a new node at the end of a dlistint_t list.
    Prototype: dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,27 +1,30 @@
 #ifndef LISTS_H
-#define LISTS_H
+#define LIST_H
+
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * dlistint_t - doublelinked list
+ * @n: numb
+ * @prev: pointer to the previous node
+ * @next: pointer to the next node
  */
-typedef struct list_s
+typedef struct dlistint_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
