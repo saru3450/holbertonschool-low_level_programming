c00c926f1e3ca61a799a432e403155c0a3388e0a
saru3450 <9971@holbertonstudents.com>
2024-12-06 16:58:14 +0000

    1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,44 +1,37 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
 
 /**
- * create_file - function that creates a file
- * @filename: name of the file to create
- * @text_content: the content to be written into the file
- * Return: 1 on success, -1 on failure
+ * create_file - create a new file
+ * @filename : name of the file we want read
+ * @text_content : content of the text
+ *
+ * Return: 1 or -1 (if fail)
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	char *p;
+	int nd, contenu;
+	int x = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	nd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (nd == -1)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		p = text_content;
-		while (*p != '\0')
+		while (text_content[x] != '\0')
+			x++;
+
+		contenu = write(nd, text_content, x);
+		if (contenu == -1)
 		{
-			if (write(fd, p, 1) != 1)
-			{
-				close(fd);
-				return (-1);
-			}
-			p++;
+			return (-1);
 		}
 	}
-	else
-	{
-		close(fd);
-		return (-1);
-	}
-	close(fd);
+	close(nd);
 	return (1);
 }
