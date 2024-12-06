f022d57226e635265954e03b3026ecdc13aaede8
saru3450 <9971@holbertonstudents.com>
2024-12-06 15:44:40 +0000

    3. Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,37 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 #include <string.h>
 
 /**
- * add_node - add new nodes at the begining
- * @head: first node
- * @str: string added
- * Return: the address of the new element, or NULL if it failed
+ * add_node_end - add a new node at the beginning of a list
+ * @head : the content of element
+ * @str : chain of chara
+ *
+ * Return: node add end
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *add, *tmp;
-        unsigned int i = 0, count = 0;
+	list_t *new = malloc(sizeof(list_t));
+	list_t *end = *head;
+	char *cop = strdup(str);
+	int i = 0;
 
-        add = malloc(sizeof(list_t));
-        if (add == NULL)
-                return (NULL);
-        add->str = strdup(str);
-        for (i = 0; str[i] != '\0'; i++)
-                count++;
-        add->len = count;
-        add->next = NULL;
-        tmp = *head;
+	if (str == NULL || new == NULL)
+		return (NULL);
 
-        if (tmp == NULL)
-                *head = add;
-        else
-        {
-                while (tmp->next != NULL)
-                        tmp = tmp->next;
-                tmp->next = add;
-        }
-        return (*head);
+	while (str[i])
+		i++;
+
+	new->str = cop;
+	new->len = i;
+	new->next = NULL;
+
+	if (*head)
+	{
+		while (end->next != NULL)
+			end = end->next;
+
+	end->next = new;
+	}
+	else
+		*head = new;
+	return (new);
 }
